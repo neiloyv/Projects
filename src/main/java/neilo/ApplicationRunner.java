bf60528b8f67c70d2c1a7b946aa1e7a44465cca8
NeiloYaroslav <neiloyv@gmail.com>
2023-10-26 17:09:47 +0300

    Application runner added

---
+++ src/main/java/neilo/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package neilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+
+        System.out.println("Success started!!!");
+        System.out.println("======================================================");
+    }
+}
