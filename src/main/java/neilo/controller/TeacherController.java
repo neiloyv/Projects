8b56887b768101bc209f786f52a25b3c3e702282
NeiloYaroslav <neiloyv@gmail.com>
2023-11-02 14:36:14 +0200

    Corrected add teacher method

--- src/main/java/neilo/controller/TeacherController.java
+++ src/main/java/neilo/controller/TeacherController.java
@@ -5,26 +5,25 @@ import neilo.dto.TeacherDto;
 import neilo.service.TeacherService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/teachers")
 public class TeacherController {
 
     private final TeacherService teacherService;
 
-    @GetMapping("/teachers")
+    @GetMapping
     public String getTeachers(Model model) {
         List<TeacherDto> teachers = teacherService.findAll();
         model.addAttribute("teachers", teachers);
         return "teachers";
     }
 
-    @PostMapping("/")
+    @PostMapping("/add")
     public String addTeacher(Model model, @ModelAttribute TeacherDto teacherDto) {
         teacherService.save(teacherDto);
         return "redirect:/teachers";
